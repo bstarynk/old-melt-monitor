a2a01291f07429a354b30d986f46885cd0987c7b
Basile Starynkevitch <basile@starynkevitch.net>
2014-04-11 23:14:09 +0200

    added json_name_item

--- monimelt-names.h
+++ monimelt-names.h
@@ -1,8 +1,9 @@
 // generated header monimelt-names.h for named items of Monimelt
 #ifndef MONIMELT_NAMED
 #error should have defined MONIMELT_NAMED macro
-#endif /*MONIMELT_NAMED */
+#endif /*MONIMELT_NAMED*/
 
-MONIMELT_NAMED (jtype, json_name, "d1f11920-92a6-40e3-bcf9-bbfe23958d5a")
+ MONIMELT_NAMED(json_name_item, json_name, "3295feb6-b16c-45f2-a1fd-fd97bf6bb7b6")
+
 // end of 1 named items
 #undef MONIMELT_NAMED
--- state-monimelt.sql
+++ state-monimelt.sql
@@ -1,6 +1,7 @@
--- state-monimelt dump 2014 Apr 11 23:02:32 MEST
+-- state-monimelt dump 2014 Apr 11 23:03:55 MEST
 CREATE TABLE t_item (uid CHAR(40) PRIMARY KEY ASC NOT NULL UNIQUE, type VARCHAR(60) NOT NULL, jbuild TEXT, jfill TEXT);
 CREATE TABLE t_name (name TEXT PRIMARY KEY ASC NOT NULL UNIQUE, nuid CHAR(40) UNIQUE NOT NULL REFERENCES t_id(uid));
+-- state-monimelt tables contents
 INSERT INTO t_item VALUES('d1f11920-92a6-40e3-bcf9-bbfe23958d5a','json_name',NULL,NULL);
 INSERT INTO t_name VALUES('jtype','d1f11920-92a6-40e3-bcf9-bbfe23958d5a');
 -- state-monimelt end dump 
