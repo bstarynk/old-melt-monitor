b01f69f26ade099500ca649434bedd764fd772cd
Basile Starynkevitch <basile@starynkevitch.net>
2014-04-12 05:19:35 +0200

    created jtype

--- monimelt-names.h
+++ monimelt-names.h
@@ -3,5 +3,7 @@
 #error should have defined MONIMELT_NAMED macro
 #endif /*MONIMELT_NAMED*/
 
+ MONIMELT_NAMED (jtype, json_name, "b9664990-11a1-49a5-bf59-be714626ed6d")
 
+// end of 1 named items
 #undef MONIMELT_NAMED
--- state-monimelt.sql
+++ state-monimelt.sql
@@ -1,5 +1,7 @@
--- state-monimelt dump 2014 Apr 12 05:10:18 MEST
+-- state-monimelt dump 2014 Apr 12 05:17:30 MEST
 CREATE TABLE t_item (uid CHAR(40) PRIMARY KEY ASC NOT NULL UNIQUE, type VARCHAR(60) NOT NULL, jbuild TEXT, jfill TEXT);
 CREATE TABLE t_name (name TEXT PRIMARY KEY ASC NOT NULL UNIQUE, nuid CHAR(40) UNIQUE NOT NULL REFERENCES t_id(uid));
 -- state-monimelt tables contents
+INSERT INTO t_item VALUES('b9664990-11a1-49a5-bf59-be714626ed6d','json_name',NULL,NULL);
+INSERT INTO t_name VALUES('jtype','b9664990-11a1-49a5-bf59-be714626ed6d');
 -- state-monimelt end dump 
