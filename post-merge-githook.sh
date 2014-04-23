53cf78f31a8b2b2d759de4ce2260893fb313f108
Basile Starynkevitch <basile@starynkevitch.net>
2014-04-23 09:25:59 +0200

    adding post-merge-githook.sh

---
+++ post-merge-githook.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -x
+# file post-merge-githook.sh
+
+# This hook is invoked by git merge, which happens when a git pull is
+# done on a local repository.The hook takes a single parameter, a
+# status flag specifying whether or not the merge being done was a
+# squash merge.
+gitstatus=$1
+
+echo post-merge-githook.sh gitstatus $gitstatus
+mv -v state-monimelt.dbsqlite state-monimelt.dbsqlite~
+sqlite3 -batch -bail state-monimelt.dbsqlite < state-monimelt.sql
--- pre-commit-githook.sh
+++ pre-commit-githook.sh
@@ -1,2 +1,6 @@
+#!/bin/sh -x
 # file pre-commit-githook.sh
+
+# This hook is invoked by git commit. It takes no parameter, and is
+# invoked after a commit is made.
 ./dump-state.sh
