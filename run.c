9b9dce411aae40c7970ff327c818c0450966e163
Basile Starynkevitch <basile@starynkevitch.net>
2014-05-24 16:15:35 +0200

    unimplemented run_one_tasklet_mom

--- run.c
+++ run.c
@@ -24,13 +24,19 @@
 struct workdata_mom_st
 {
   unsigned work_magic;		/* always WORK_MAGIC */
-  unsigned work_index;
+  uint8_t work_index;
+  bool work_running;
   pthread_t work_thread;
 };
 
+static bool stop_working_mom;
 static __thread struct workdata_mom_st *cur_worker_mom;
+static struct workdata_mom_st work_data_array_mom[MOM_MAX_WORKERS + 1];
+static int64_t task_counter_mom;
 // this cond is tied to the agenda's mutex
 static pthread_cond_t agenda_cond_mom = PTHREAD_COND_INITIALIZER;
+
+static void run_one_tasklet_mom (momitem_t *tskitm);
 
 void
 mom_add_tasklet_to_agenda_back (momitem_t *tkitm)
@@ -68,4 +74,95 @@ mom_add_tasklet_to_agenda_front (momitem_t *tkitm)
   if (agendawasempty)
     pthread_cond_broadcast (&agenda_cond_mom);
   pthread_mutex_unlock (&mom_named__agenda->i_mtx);
+}
+
+static void *
+work_run_mom (void *p)
+{
+  assert ((char *) p > (char *) work_data_array_mom
+	  && (char *) p <= (char *) (work_data_array_mom + MOM_MAX_WORKERS));
+  assert (mom_named__agenda != NULL
+	  && mom_named__agenda->i_typnum == momty_item);
+  struct workdata_mom_st *thiswork = (struct workdata_mom_st *) p;
+  unsigned wix = thiswork - work_data_array_mom;
+  assert (wix > 0 && wix <= MOM_MAX_WORKERS
+	  && wix <= (unsigned) mom_nb_workers);
+  assert (p == (void *) (work_data_array_mom + wix));
+  {
+    char thrname[24];
+    memset (thrname, 0, sizeof (thrname));
+    snprintf (thrname, sizeof (thrname), "mom-work%02d", wix);
+    pthread_setname_np (pthread_self (), thrname);
+  }
+  thiswork->work_index = wix;
+  cur_worker_mom = thiswork;
+  MOM_DEBUGPRINTF (run, "work_run_mom: begin wix#%d", wix);
+  uint64_t wcount = 0;
+  bool again = true;
+  usleep (10 + mom_random_32 () % 256);
+  while (again)
+    {
+      bool agendaempty = false;
+      pthread_mutex_lock (&mom_named__agenda->i_mtx);
+      if (MOM_UNLIKELY (mom_named__agenda->i_paylkind != mompayk_queue))
+	mom_item_start_queue (mom_named__agenda);
+      agendaempty = mom_item_queue_is_empty (mom_named__agenda);
+      if (stop_working_mom)
+	{
+	  thiswork->work_running = false;
+	  again = false;
+	  MOM_DEBUGPRINTF (run, "work_run_mom: stop working wix#%d", wix);
+	}
+      else if (agendaempty)
+	{
+	  thiswork->work_running = false;
+	  double delay = (MOM_IS_DEBUGGING (run) ? 3.7 : 1.5)
+	    + 0.001 * (mom_random_32 () % 256);
+	  struct timespec ts
+	    = mom_timespec (mom_clock_time (CLOCK_REALTIME) + delay);
+	  MOM_DEBUGPRINTF (run, "work_run_mom: waiting agenda cond wix#%d",
+			   wix);
+	  pthread_cond_timedwait (&agenda_cond_mom, &mom_named__agenda->i_mtx,
+				  &ts);
+	  MOM_DEBUGPRINTF (run, "work_run_mom: waited agenda cond wix#%d",
+			   wix);
+	}
+      else
+	{
+	  momitem_t *curtskitm =
+	    mom_value_to_item (mom_item_queue_pop_front (mom_named__agenda));
+	  MOM_DEBUG (run, MOMOUT_LITERAL ("work_run_mom: wix#"),
+		     MOMOUT_DEC_INT ((int) wix),
+		     MOMOUT_LITERAL (", curtskitm="),
+		     MOMOUT_ITEM ((const momitem_t *) curtskitm));
+	  if (curtskitm)
+	    {
+	      thiswork->work_running = true;
+	      wcount++;
+	      task_counter_mom++;
+	      MOM_DEBUGPRINTF (run,
+			       "work_run_mom: wix#%d wcount=%ld task_counter_mom=%lld",
+			       wix, (long) wcount,
+			       (long long) task_counter_mom);
+	      pthread_mutex_unlock (&mom_named__agenda->i_mtx);
+	      run_one_tasklet_mom (curtskitm);
+	      continue;
+	    }
+	}
+      pthread_mutex_unlock (&mom_named__agenda->i_mtx);
+    }				/* end while again */
+  cur_worker_mom = NULL;
+  return NULL;
+}
+
+
+void
+run_one_tasklet_mom (momitem_t *tkitm)
+{
+  MOM_DEBUG (run, MOMOUT_LITERAL ("run_one_tasklet_mom start tkitm:"),
+	     MOMOUT_ITEM ((const momitem_t *) tkitm));
+  pthread_mutex_lock (&tkitm->i_mtx);
+  MOM_FATAPRINTF ("unimplemented run_one_tasklet_mom");
+#warning unimplemented run_one_tasklet_mom
+  pthread_mutex_unlock (&tkitm->i_mtx);
 }
