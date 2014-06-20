d8ad332e1744f032db44e818a95a00fe5ecd2058
Basile Starynkevitch <basile@starynkevitch.net>
2014-06-20 11:13:33 +0200

    adding more in update_display_value

--- routines.c
+++ routines.c
@@ -2435,6 +2435,11 @@ enum update_display_value_valindex_en
   update_display_value_v_display,
   update_display_value_v_spare,
   update_display_value_v_dispnode,
+  update_display_value_v_dispconn,
+  update_display_value_v_curitem,
+  update_display_value_v_curattr,
+  update_display_value_v_subdisplay,
+  update_display_value_v_curval,
   update_display_value_v__lastval
 };
 
@@ -2462,31 +2467,90 @@ update_display_value_codmom (int momstate_, momitem_t *momtasklet_,
   {
     update_display_value_s_start,
     update_display_value_s_impossible,
+    update_display_value_s_didupdateattrdisp,
     update_display_value_s__laststate
   };
-#define _SET_STATE(St) do {					\
-    MOM_DEBUGPRINTF (run,					\
-		     "update_display_value_codmom setstate " #St " = %d",	\
-		     (int)update_display_value_s_##St);				\
+#define _SET_STATE(St) do {						\
+    MOM_DEBUGPRINTF (run,						\
+		     "update_display_value_codmom setstate " #St " = %d", \
+		     (int)update_display_value_s_##St);			\
     return update_display_value_s_##St; } while(0)
   if (momstate_ >= 0 && momstate_ < update_display_value_s__laststate)
     switch ((enum update_display_value_state_en) momstate_)
       {
       case update_display_value_s_start:
 	goto update_display_value_lab_start;
+	///
+      case update_display_value_s_didupdateattrdisp:
+	goto update_display_value_lab_didupdateattrdisp;
+	///
       case update_display_value_s_impossible:
 	goto update_display_value_lab_impossible;
+	///
       case update_display_value_s__laststate:;
       }
   MOM_FATAPRINTF ("update_display_value invalid state #%d", momstate_);
 update_display_value_lab_start:
-  MOM_DEBUG (run, MOMOUT_LITERAL ("update_display_value start display="),
-	     MOMOUT_VALUE ((const momval_t) _L (display)),
-	     MOMOUT_LITERAL (" !: "),
-	     MOMOUT_ITEM_ATTRIBUTES ((const momitem_t *)
-				     mom_value_to_item (_L (display))));
   if (_L (display).ptr == MOM_EMPTY)
     _SET_STATE (impossible);
+  {
+    mom_should_lock_item (_L (display).pitem);
+    MOM_DEBUG (run, MOMOUT_LITERAL ("update_display_value start display="),
+	       MOMOUT_VALUE ((const momval_t) _L (display)),
+	       MOMOUT_LITERAL (" !: "),
+	       MOMOUT_ITEM_ATTRIBUTES ((const momitem_t *)
+				       mom_value_to_item (_L (display))),
+	       NULL);
+    _L (dispnode) =
+      mom_item_get_attribute (_L (display).pitem, mom_named__display);
+    mom_unlock_item (_L (display).pitem);
+  }
+  _L (dispconn) = (momval_t) mom_node_conn (_L (dispnode));
+  if (_L (dispconn).pitem == mom_named__attr
+      && mom_node_arity (_L (dispnode)) == 3)
+    {
+      _L (curitem) = mom_node_nth (_L (dispnode), 0);
+      _L (curattr) = mom_node_nth (_L (dispnode), 1);
+      _L (subdisplay) = mom_node_nth (_L (dispnode), 2);
+      MOM_DEBUG (run,
+		 MOMOUT_LITERAL
+		 ("update_display_value start attr subdisplay="),
+		 MOMOUT_VALUE ((const momval_t) _L (subdisplay)),
+		 MOMOUT_LITERAL (" !: "),
+		 MOMOUT_ITEM_ATTRIBUTES ((const momitem_t *)
+					 mom_value_to_item (_L (subdisplay))),
+		 NULL);
+
+      mom_item_tasklet_push_frame (momtasklet_, (momval_t) momclosure_,
+				   MOMPFR_VALUE (_L (subdisplay)), NULL);
+      mom_item_tasklet_clear_res (momtasklet_);
+      _SET_STATE (didupdateattrdisp);
+      ////// ********
+    update_display_value_lab_didupdateattrdisp:
+      _L (curval) = mom_item_tasklet_res1 (momtasklet_);
+      mom_item_tasklet_clear_res (momtasklet_);
+      MOM_DEBUG (run,
+		 MOMOUT_LITERAL
+		 ("update_display_value didupdateattrdisp curval="),
+		 MOMOUT_VALUE ((const momval_t) _L (curval)),
+		 MOMOUT_LITERAL (" curitem="),
+		 MOMOUT_VALUE ((const momval_t) _L (curitem)),
+		 MOMOUT_LITERAL (" curattr="),
+		 MOMOUT_VALUE ((const momval_t) _L (curattr)), NULL);
+      {
+	mom_should_lock_item (_L (curitem).pitem);
+	mom_item_put_attribute (_L (curitem).pitem, _L (curattr).pitem,
+				_L (curval));
+	mom_unlock_item (_L (curitem).pitem);
+      }
+      return momroutres_pop;
+    }
+  else
+    MOM_FATAL (MOMOUT_LITERAL
+	       ("update_display_value fail to handle dispnode="),
+	       MOMOUT_VALUE ((const momval_t) _L (dispnode)),
+	       MOMOUT_LITERAL (" in display="), MOMOUT_VALUE (_L (display)),
+	       NULL);
   return momroutres_pop;
 update_display_value_lab_impossible:
   MOM_FATAPRINTF ("update_display_value impossible state reached!");
