38243911e649645cad38818e295777c642903f98
Basile Starynkevitch <basile@starynkevitch.net>
2014-11-14 13:53:46 +0100

    adding command skeletons

--- momplug_cmd.c
+++ momplug_cmd.c
@@ -32,40 +32,41 @@ const char mom_plugin_GPL_compatible[] = "GPLv3+";
 // the command stack has values and marks
 static momval_t *vst_valarr_mom;
 static char *vst_markarr_mom;
-static unsigned vst_size;
-static unsigned vst_top;
+static unsigned vst_size_mom;
+static unsigned vst_top_mom;
+
 
 static void
 cmd_stack_push_mom (momval_t val, char mark)
 {
-  if (vst_top >= vst_size)
+  if (vst_top_mom >= vst_size_mom)
     {
-      unsigned newsiz = (4 * vst_size / 3 + 21) | 0x1f;
+      unsigned newsiz = (4 * vst_size_mom / 3 + 21) | 0x1f;
       momval_t *newvalarr =
 	MOM_GC_ALLOC ("valarr", newsiz * sizeof (momval_t));
       char *newmarkarr = MOM_GC_SCALAR_ALLOC ("markarr", newsiz + 1);
-      if (vst_top > 0)
+      if (vst_top_mom > 0)
 	{
-	  memcpy (newvalarr, vst_valarr_mom, vst_top * sizeof (momval_t));
-	  memcpy (newmarkarr, vst_markarr_mom, vst_top);
+	  memcpy (newvalarr, vst_valarr_mom, vst_top_mom * sizeof (momval_t));
+	  memcpy (newmarkarr, vst_markarr_mom, vst_top_mom);
 	  MOM_GC_FREE (vst_valarr_mom);
 	  MOM_GC_FREE (vst_markarr_mom);
 	}
       vst_valarr_mom = newvalarr;
       vst_markarr_mom = newmarkarr;
     }
-  vst_valarr_mom[vst_top] = val;
-  vst_markarr_mom[vst_top] = mark;
-  vst_top++;
+  vst_valarr_mom[vst_top_mom] = val;
+  vst_markarr_mom[vst_top_mom] = mark;
+  vst_top_mom++;
 }
 
 static inline momval_t
 cmd_stack_nth_value_mom (int rk)
 {
   if (rk < 0)
-    rk += vst_top;
-  if (rk >= 0 && rk < (int) vst_top)
-    return vst_valarr_mom[vst_top - rk];
+    rk += vst_top_mom;
+  if (rk >= 0 && rk < (int) vst_top_mom)
+    return vst_valarr_mom[vst_top_mom - rk];
   return MOM_NULLV;
 }
 
@@ -73,20 +74,20 @@ static inline char
 cmd_stack_nth_mark_mom (int rk)
 {
   if (rk < 0)
-    rk += vst_top;
-  if (rk >= 0 && rk < (int) vst_top)
-    return vst_markarr_mom[vst_top - rk];
+    rk += vst_top_mom;
+  if (rk >= 0 && rk < (int) vst_top_mom)
+    return vst_markarr_mom[vst_top_mom - rk];
   return 0;
 }
 
 static void
 cmd_stack_pop_mom (unsigned nb)
 {
-  if (nb < vst_top && nb > 0)
+  if (nb < vst_top_mom && nb > 0)
     {
-      memset (vst_valarr_mom + vst_top - nb, 0, sizeof (momval_t) * nb);
-      memset (vst_markarr_mom + vst_top - nb, 0, sizeof (char) * nb);
-      vst_top -= nb;
+      memset (vst_valarr_mom + vst_top_mom - nb, 0, sizeof (momval_t) * nb);
+      memset (vst_markarr_mom + vst_top_mom - nb, 0, sizeof (char) * nb);
+      vst_top_mom -= nb;
     }
 }
 
@@ -100,15 +101,27 @@ cmd_stack_pop_mom (unsigned nb)
   CMD(top,"print the top of stack")		\
 				/* end of COMMANDS */
 
-static const char *cmdarr_mom[] = {
-#define CMD_NAME(N,C) #N,
-  COMMANDS (CMD_NAME)
-#undef CMD_NAME
-  NULL
-};
+#define CMD_DECLARE(N,H) static void cmd_do_##N##_mom (const char*);
+COMMANDS (CMD_DECLARE)
+#undef CMD_DECLARE
+     typedef void cmd_do_fun_t (const char *);
+     struct cmd_descr_st
+     {
+       const char *cmd_name;
+       cmd_do_fun_t *const cmd_fun;
+       const char *cmd_help;
+     };
 
-#define CMDARRSIZE_MOM (sizeof(cmdarr_mom)/sizeof(cmdarr_mom[0]))
-// readline alternate completion function
+     static struct cmd_descr_st cmd_array_mom[] = {
+#define CMD_DEFINE(N,H) {.cmd_name= #N, .cmd_fun= cmd_do_##N##_mom, .cmd_help= H},
+       COMMANDS (CMD_DEFINE)
+#undef CMD_DEFINE
+       {NULL, NULL, NULL}
+     };
+
+#define CMDARRSIZE_MOM (sizeof(cmd_array_mom)/sizeof(cmd_array_mom[0]))
+
+// readline completion entry function
 static char *
 cmd_completion_entry_mom (const char *text, int state)
 {
@@ -140,15 +153,16 @@ cmd_completion_entry_mom (const char *text, int state)
 	  momval_t jvals[CMDARRSIZE_MOM + 1] = { MOM_NULLV };
 	  for (int ix = 0; ix < (int) CMDARRSIZE_MOM; ix++)
 	    {
-	      if (!cmdarr_mom[ix])
+	      if (!cmd_array_mom[ix].cmd_name)
 		break;
-	      if (!strncmp (text + 1, cmdarr_mom[ix], cmdlen))
+	      if (!strncmp (text + 1, cmd_array_mom[ix].cmd_name, cmdlen))
 		{
 		  jvals[nbent] =
-		    (momval_t) MOM_OUTSTRING (0, MOMOUT_LITERAL (","),
-					      MOMOUT_LITERALV ((const char *)
-							       cmdarr_mom
-							       [ix]));
+		    (momval_t)
+		    MOM_OUTSTRING (0, MOMOUT_LITERAL (","),
+				   MOMOUT_LITERALV ((const char *)
+						    cmd_array_mom
+						    [ix].cmd_name));
 		  nbent++;
 		}
 	    }
@@ -166,7 +180,9 @@ cmd_completion_entry_mom (const char *text, int state)
 	    return NULL;
 	}
       else if (text[0] == '_' && isalnum (text[1]) && isalnum (text[2]))
-	{			// a line starting with some id-like thing is tab-expanded to the set of items of id prefixed by the text       
+	{
+	  // a line starting with some id-like thing is tab-expanded
+	  // to the set of items of id prefixed by the text
 	  jarr = MOM_NULLV;
 	  momval_t setv =
 	    (momval_t) mom_set_of_items_of_ident_prefixed (text);
@@ -198,6 +214,61 @@ cmd_completion_entry_mom (const char *text, int state)
   return NULL;
 }
 
+
+////////////////////////////////////////////////////////////////
+static void
+cmd_do_dump_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_dump lin=%s", lin);
+}
+
+
+static void
+cmd_do_dup_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_dup lin=%s", lin);
+}
+
+static void
+cmd_do_exit_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_exit lin=%s", lin);
+}
+
+
+static void
+cmd_do_help_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_help lin=%s", lin);
+  putchar ('\n');
+  for (struct cmd_descr_st * cd = cmd_array_mom;
+       cd != NULL && cd->cmd_name != NULL; cd++)
+    printf (" %s: %s\n", cd->cmd_name, cd->cmd_help);
+  putchar ('\n');
+  fflush (NULL);
+}
+
+static void
+cmd_do_quit_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_quit lin=%s", lin);
+}
+
+static void
+cmd_do_stack_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_stack lin=%s", lin);
+}
+
+static void
+cmd_do_top_mom (const char *lin)
+{
+  MOM_DEBUGPRINTF (cmd, "start do_top lin=%s", lin);
+}
+
+
+////////////////////////////////////////////////////////////////
+
 void
 mom_plugin_init (const char *arg)
 {
@@ -223,7 +294,7 @@ momplugin_after_load (void)
       MOM_DEBUGPRINTF (cmd, "before readline prompt=%s", prompt);
       lin = readline (prompt);
       MOM_DEBUGPRINTF (cmd, "after readline lin=%s", lin);
-      if (!lin || !strcmp (lin, ",quit"))
+      if (!lin)
 	break;
     };
   MOM_INFORMPRINTF ("momplug_cmd ending after load");
