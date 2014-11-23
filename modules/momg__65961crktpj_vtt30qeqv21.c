6f429e2328a77e4c1685e33b6da05ddaaa6cffd0
Basile Starynkevitch <basile@starynkevitch.net>
2014-11-23 18:06:27 +0100

    adding manually modules/momg__65961crktpj_vtt30qeqv21.c

---
+++ modules/momg__65961crktpj_vtt30qeqv21.c
@@ -0,0 +1,125 @@
+// generated MONIMELT module file momg__65961crktpj_vtt30qeqv21.c
+// DO NOT EDIT
+
+
+///// declarations
+// generated monimelt module file momg__65961crktpj_vtt30qeqv21.c ** DO NOT EDIT **
+
+////////////////////////////////////////////////////////////////++
+
+/// generated file momg__65961crktpj_vtt30qeqv21.c ** DO NOT EDIT
+
+/// Copyright (C) 2014 Free Software Foundation, Inc.
+// MONIMELT is a monitor for MELT - see http://gcc-melt.org/
+// This file is part of GCC.
+
+// GCC is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+
+// GCC is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with GCC; see the file COPYING3.   If not see
+//  <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////--
+
+
+
+#include "monimelt.h"
+
+////++++ declaration of 1 routines:
+
+// declare tasklet function ajax_appl rank#0
+static int momfuncod__06uk4pppvx9_huv0v11v18j(int, momitem_t*, momval_t, momval_t*, intptr_t*, double*);
+
+static momitem_t* momfconstitems__06uk4pppvx9_huv0v11v18j[1]; // constant items of tasklet function ajax_appl
+
+// declare module md5sum for first_module
+const char mommd5mod__65961crktpj_vtt30qeqv21[] = MONIMELT_MD5_MODULE; // Makefile generated
+
+// declare module routines descriptor array for first_module
+static const union momrout_un momdroutarr__65961crktpj_vtt30qeqv21[2];
+
+
+
+//// implementations
+
+
+////++++ implementation of 1 routines:
+
+
+// implement tasklet function ajax_appl rank#0
+static int momfuncod__06uk4pppvx9_huv0v11v18j
+(int momstate,
+	 momitem_t* restrict momtasklet, const momval_t momclosure,
+	 momval_t* restrict momvals,
+	 intptr_t* restrict momnums_ MOM_UNUSED,
+	 double* restrict momdbls_ MOM_UNUSED)
+{ // start of tasklet function ajax_appl
+ // declared 1 locals, 1 arguments.
+ if (MOM_UNLIKELY(momstate==0)) return 1;
+ assert (mom_is_item (momclosure));
+ momval_t* momclovals = mom_item_closure_values (momclosure.pitem);
+ assert (momclovals != NULL);
+ assert (mom_item_payload_kind(momtasklet)== mompayk_tasklet);
+ assert (momvals != NULL); // 2 values.
+ switch (momstate) {
+ case 1: goto momfblo_1;
+ default: return momroutres_pop;
+ }; // end switch state
+ 
+ // function block #1 _8y756mef2ca_w8cj58726vj
+ momfblo_1:
+ {/*!assign*/  momvals[2/*:whatv*/] = (/*!primitive mom_webx_post_arg*/  mom_webx_post_arg (mom_value_to_item (
+  momvals[0/*:webx*/]),  /*!litstr*/ "what_mom"));
+  
+ }; // end function block _8y756mef2ca_w8cj58726vj
+ return momroutres_pop;
+ } // end function ajax_appl
+
+static const char* const momfconstid__06uk4pppvx9_huv0v11v18j[1] = { // constant ids of function ajax_appl
+}; // end of function constants of ajax_appl
+
+const struct momtfundescr_st momrout__06uk4pppvx9_huv0v11v18j = { // tasklet function descriptor ajax_appl
+ .tfun_magic = MOM_TFUN_MAGIC,
+ .tfun_minclosize = 0,
+ .tfun_nbconstants = 0,
+ .tfun_frame_nbval = 2,
+ .tfun_frame_nbnum = 0,
+ .tfun_frame_nbdbl = 0,
+ .tfun_constantids = momfconstid__06uk4pppvx9_huv0v11v18j,
+ .tfun_constantitems = (momitem_t**) momfconstitems__06uk4pppvx9_huv0v11v18j,
+ .tfun_ident = "_06uk4pppvx9_huv0v11v18j",
+ .tfun_module = MONIMELT_CURRENT_MODULE,
+ .tfun_codefun = momfuncod__06uk4pppvx9_huv0v11v18j,
+ .tfun_timestamp = __DATE__ "@" __TIME__
+ 
+}; // end function descriptor
+
+// define module routines descriptor array for first_module
+static const union momrout_un momdroutarr__65961crktpj_vtt30qeqv21[2] = {
+ [0]= {.rtfun= &momrout__06uk4pppvx9_huv0v11v18j}, // taskletfun ajax_appl
+ 
+}; // end of module routines descriptor array for first_module
+
+
+// module initialization for first_module
+void mominitmodule__65961crktpj_vtt30qeqv21 (void) {
+ mom_module_internal_initialize ("_65961crktpj_vtt30qeqv21" /*!module first_module*/,
+       MONIMELT_MD5_MODULE /*see Makefile*/,  1,  momdroutarr__65961crktpj_vtt30qeqv21);
+ MOM_INFORMPRINTF("module first_module of md5 " MONIMELT_MD5_MODULE " initialized.");
+} // end of module initialization
+
+
+
+ // module license
+const char mom_module_GPL_compatible[]=
+	"GPLv3+, generated module _65961crktpj_vtt30qeqv21";
+
+
+
+//// end of generated file momg__65961crktpj_vtt30qeqv21.c
