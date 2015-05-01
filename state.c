60fed824c7d5c0516fb404a96717dfb7d2eac0c5
Basile Starynkevitch <basile@starynkevitch.net>
2015-05-01 09:41:09 +0200

    assert failure when running:  ./monimelt --add-predefined "function_signature" "kind of function signatures"

--- state.c
+++ state.c
@@ -2556,11 +2556,11 @@ mom_dump_state (const char *prefix)
   {
     const momseq_t *userset =
       mom_hashset_elements_set (dumper_mom->duitemuserset);
+    const momseq_t *globset =
+      mom_hashset_elements_set (dumper_mom->duitemglobalset);
     unsigned nbuseritems = userset->slen;
     for (unsigned ix = 0; ix < nbuseritems; ix++)
       mom_item_unlock ((momitem_t *) userset->arritm[ix]);
-    const momseq_t *globset =
-      mom_hashset_elements_set (dumper_mom->duitemglobalset);
     unsigned nbglobitems = globset->slen;
     for (unsigned ix = 0; ix < nbglobitems; ix++)
       mom_item_unlock ((momitem_t *) globset->arritm[ix]);
